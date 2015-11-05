a9e119b8ce10ce1eac4a64c1a8e821a57b4ac4d3
lichong <lichong@PC-20150724WQCQ>
2015-11-05 10:39:29 +0800

    Signed-off-by: lichong <lichong@PC-20150724WQCQ>

--- src/main/java/com/ynyes/huizi/util/SiteMagConstant.java
+++ src/main/java/com/ynyes/huizi/util/SiteMagConstant.java
@@ -1,4 +1,7 @@
 package com.ynyes.huizi.util;
+
+import java.util.Properties;
+
 /**
  * 后台常用常量
  * @author Sharon
@@ -10,10 +13,24 @@ public class SiteMagConstant {
        
     public static final String templatePath = "src/main/resources/templates/client/";
     
-    public static final String backupPath = "src/main/resources/backup/";
-    public static final String imagePath = "src/main/resources/static/images";
+//    public static final String backupPath = "src/main/resources/backup/";
+//    public static final String imagePath = "src/main/resources/static/images";
     
 //    public static final String backupPath = "/mnt/root/backup/";
 //    public static final String imagePath = "/mnt/root/images/";
 //     
+    public static String backupPath; 
+    public static String imagePath;
+    
+    static{
+		Properties props = System.getProperties();
+		String operation = props.getProperty("os.name");
+		if(operation.contains("Linux")){
+			backupPath = "/mnt/root/backup/";
+			imagePath = "/mnt/root/images/goods";
+		}else{
+			backupPath = "src/main/resources/backup/";
+			imagePath = "src/main/resources/static/images";
+		}
+    }
 }
