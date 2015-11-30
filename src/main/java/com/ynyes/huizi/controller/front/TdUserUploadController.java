96a0d896509081246c68afd6178c43b86654a0d2
madejing <Administrator@PC-20150701TNZB>
2015-11-30 18:40:50 +0800

    上传控件

--- src/main/java/com/ynyes/huizi/controller/front/TdUserUploadController.java
+++ src/main/java/com/ynyes/huizi/controller/front/TdUserUploadController.java
@@ -3,12 +3,15 @@ package com.ynyes.huizi.controller.front;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +24,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ynyes.huizi.service.TdArticleCategoryService;
 import com.ynyes.huizi.service.TdArticleService;
 import com.ynyes.huizi.util.SiteMagConstant;
+
+import net.sf.json.JSONObject;
 
 /**
  * 后台首页控制器
@@ -43,7 +48,7 @@ public class TdUserUploadController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ResponseBody
     public Map<String, Object> upload(String action,
-            @RequestParam MultipartFile Filedata, HttpServletRequest req) {
+            @RequestParam MultipartFile Filedata, HttpServletRequest req,HttpServletResponse response) {
         Map<String, Object> res = new HashMap<String, Object>();
         res.put("status", 0);
         String username = (String) req.getSession().getAttribute("username");
@@ -91,8 +96,28 @@ public class TdUserUploadController {
             res.put("status", 0);
             res.put("msg", "上传文件失败！");
         }
+        response.reset();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            JSONObject jsonObject = JSONObject.fromObject(res);
+            writer.println(jsonObject);  //想办法把map转成json
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (Exception e) {
+                    System.err.println(e);
+                }
+            }
+        }
 
-        return res;
+        return null;
 
     }
     @RequestMapping(value = "/upload/client", method = RequestMethod.POST)
